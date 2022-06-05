64bfea58e2e7e22cdd324cb360ca42b89288270c
samikshyakc1996 <samikshyakc1996@gmail.com>
2022-06-04 22:37:00 -0400

    added contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./contact.scss"
 function Contact() {
+
+  const [message,setMessage]=useState(false);
+  const handleFormSubmit=(e)=>{
+    e.preventDefault();
+    console.log("form is submitted");
+    setMessage(true);
+
+  }
   return (
     <div className='contact' id='contact'>
-        hello from contact
+        <div className="left">
+          <img src="assets/shake.svg" alt="hand shake svg" />
+        </div>
+        <div className="right">
+          <form  onSubmit={handleFormSubmit}>
+            <input type="text"  placeholder='Subject'/>
+            <textarea placeholder='Type your message' cols="30" rows="10"></textarea>
+            <button type='submit'>Send</button>
+            {message && <span>Thanks, I will reply ASAP.</span>}
+          </form>
+
+        </div>
     </div>
   )
 }
